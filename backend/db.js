570c8dfa93b369f6fb7a9739d099955a38ffc41a
Niranjan <2100090187@kluniversity.in>
2024-01-20 00:02:12 +0530

    connected database

--- backend/db.js
+++ backend/db.js
@@ -1,11 +1,19 @@
 const mongoose = require('mongoose');
 const mongoURI="mongodb://localhost:27017/";
-
+const dbName = 'loginSystem'
 
 
 const connectToMongo = async ()=>{
     try {
-        await mongoose.connect(mongoURI, )
+        await mongoose.connect(mongoURI,{ useNewUrlParser: true },(err,client)=>{
+            if(err)
+            {
+                console.log("Error connecting to MongoDB: ",err.message);
+            }
+            db=client.db(dbName);
+            console.log(`Connected MongoDB: ${url}`)
+            console.log(`Database: ${dbName}`)
+        } )
         
     } catch (error) {
         console.error('Error connecting to MongoDB: ', error.message);
@@ -13,3 +21,6 @@ const connectToMongo = async ()=>{
     }
 }
 module.exports = connectToMongo;
+
+
+
--- backend/index.js
+++ backend/index.js
@@ -1,23 +1,29 @@
 const connectToMongoose = require('./db');
-const express= require('express');
-connectToMongoose();
+const express = require('express');
+const cors = require('cors'); // Import cors
 
+const corsMiddleware = require('./middleware/corsMiddleware'); // Import your custom corsMiddleware
+
+const app = express();
+const port = 5000;
 
 // Use cors middleware
 app.use(corsMiddleware);
 
-const app = express();
-const cors = require('cors');
-const port=5000;
-app.listen(port,(error)=>{
-    if (!error) {
-        console.log(`Server is running at port ${port}`);
-        
-    }
-    else{
-        console.log("Error occured while listening on port ${port",error);
-    }
-});
+// Other middleware and routes go here
+app.use('/login',require('./routes/auth'));
+app.use('/signup',require('./routes/auth'));
+
+
+
 app.get('/', (req, res) => {
-    res.send('Hello World!')
-  })
+  res.send('Hello World!');
+});
+
+app.listen(port, (error) => {
+  if (!error) {
+    console.log(`Server is running at port ${port}`);
+  } else {
+    console.log(`Error occurred while listening on port ${port}`, error);
+  }
+});
