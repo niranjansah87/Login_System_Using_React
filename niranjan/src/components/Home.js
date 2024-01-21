2bb25d1b134d5d0496da8c8e3fa17a70b1420efe
Niranjan <2100090187@kluniversity.in>
2024-01-21 23:40:58 +0530

    enough for today

--- niranjan/src/components/Home.js
+++ niranjan/src/components/Home.js
@@ -1,11 +1,51 @@
-import React from 'react'
+// import React from 'react'
 // import Navbar from './Navbar'
 import '../assets/home.css'
+import AuthNavbar from './AuthNavbar'; // Import the 'AuthNavbar' component
+import UnAuthNavbar from './UnAuthNavbar'; // Import the 'UnAuthNavbar' component
+import React, { useState,useEffect } from 'react'; // Import the 'useState' hook from the 'react' package
+
 export default function Home() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  useEffect(() => {
+    const checkLoginStatus = async () => {
+      try {
+        const authToken = localStorage.getItem('authToken');
+        console.log('Stored Auth Token:', authToken);
+
+        const response = await fetch('http://localhost:5000/api/auth/checklogin', {
+          method: 'GET',
+          credentials: 'include',
+          headers: {
+            'auth-token': authToken, // Corrected header name
+          },
+        });
+        console.log('Full response:', response);
+        if (response.ok) {
+          setIsLoggedIn(true);
+        } else {
+          setIsLoggedIn(false);
+        }
+      } catch (error) {
+        console.error('Check login status error:', error.message);
+        setIsLoggedIn(false);
+      }
+    };
+
+    checkLoginStatus();
+  }, []);
   return (
     <>
-    <h1>Niranjan</h1>
-    
+      <div>
+        {isLoggedIn ? (
+          <>
+            <AuthNavbar />
+            <h1>My name is Niranjan Sah</h1>
+          </>
+        ) : (
+          <UnAuthNavbar />
+        )}
+      </div>
     </>
   )
 }
--- niranjan/src/components/Profile.js
+++ niranjan/src/components/Profile.js
@@ -1,58 +1,163 @@
-import React, { useState, useEffect } from 'react';
+// // // import React, { useState, useEffect } from 'react';
+// // import React from 'react';
+// // import { Link } from 'react-router-dom';
+// // // import '../assets/profile.css'
+// // import styles from '../assets/profile.module.css';
+// // console.log(styles);
+// // export default function Profile() {
+// //   console.log(styles.update_profile);
+// //   // const [userData, setUserData] = useState({
+// //   //   name: '',
+// //   //   email: '',
+// //   //   contact: '',
+// //   // });
+
+// //   // useEffect(() => {
+// //   //   const fetchUserData = async () => {
+// //   //     try {
+// //   //       const response = await fetch('http://localhost:5000/api/auth/getuser', {
+// //   //         method: 'POST',
+// //   //         credentials: 'include', // Include credentials if using cookies for authentication
+// //   //       });
+
+// //   //       if (response.ok) {
+// //   //         const user = await response.json();
+// //   //         setUserData(user);
+// //   //       } else {
+// //   //         console.error('Failed to fetch user data:', response.statusText);
+// //   //       }
+// //   //     } catch (error) {
+// //   //       console.error('Fetch user data error:', error.message);
+// //   //     }
+// //   //   };
+
+// //   //   fetchUserData();
+// //   // }, []); // Empty dependency array ensures the effect runs only once when the component mounts
+
+// //   return (
+// //     <div styles={{background: url("./login.jpg");,
+// //     background-repeat: no-repeat;,
+// //     background-size: cover;}}>
+// //     <div className={styles.update_profile}>
+// //       <h1>Profile</h1>
+// //       <form  action="#" encType="multipart/form-data">
+// //         <div className={styles.flex}>
+// //           <div className={styles.inputBox}>
+// //             <span>Full name :</span>
+// //             {/* <input type="text" name="update_name" value={userData.name} className="{styles.box}" disabled /> */}
+// //             <input type="text" name="update_name"  className={styles.box} disabled />
+// //             <span>Email :</span>
+// //             {/* <input type="email" name="update_email" value={userData.email} className={styles.box disabled /> */}
+// //             <input type="email" name="update_email" className={styles.box} disabled />
+// //           </div>
+
+// //           <div className={styles.inputBox}>
+// //             <span>Contact Number</span>
+// //             {/* <input value={userData.contact} className="{styles.box}" disabled /> */}
+// //             <input className={styles.box} disabled />
+// //           </div>
+// //         </div>
+
+// //         <Link to="/index" className={styles.delete_btn}>
+// //           Go back
+// //         </Link>
+// //       </form>
+// //     </div>
+// //     </div>
+// //   );
+// // }
+
+
+
+
+// // import React, { useState, useEffect } from 'react';
+// import React from 'react';
+// import { Link } from 'react-router-dom';
+// // import '../assets/profile.css'
+// import styles from '../assets/profile.module.css';
+// // console.log(styles);
+
+// export default function Profile() {
+//   // console.log(styles.update_profile);
+//   const myStyle = {
+//     background: `url("../assets/login.jpg"")`,
+//       backgroundRepeat: 'no-repeat',
+//       backgroundSize: 'cover',
+// };
+//   return (
+//     <div className={myStyle}>
+//       <div className={styles.update_profile}>
+//         <h1>Profile</h1>
+//         <form action="#" encType="multipart/form-data">
+//           <div className={styles.flex}>
+//             <div className={styles.inputBox}>
+//               <span>Full name :</span>
+//               <input type="text" name="update_name" className={styles.box} disabled />
+//               <span>Email :</span>
+//               <input type="email" name="update_email" className={styles.box} disabled />
+//             </div>
+
+//             <div className={styles.inputBox}>
+//               <span>Contact Number</span>
+//               <input className={styles.box} disabled />
+//             </div>
+//           </div>
+
+//           <Link to="/index" className={styles.delete_btn}>
+//             Go back
+//           </Link>
+//         </form>
+//       </div>
+//     </div>
+//   );
+// }
+
+
+
+
+import React from 'react';
 import { Link } from 'react-router-dom';
+// import styles from '../assets/profile.module.css';
 
 export default function Profile() {
-  const [userData, setUserData] = useState({
-    name: '',
-    email: '',
-    contact: '',
-  });
-
-  useEffect(() => {
-    const fetchUserData = async () => {
-      try {
-        const response = await fetch('http://localhost:5000/api/auth/getuser', {
-          method: 'POST',
-          credentials: 'include', // Include credentials if using cookies for authentication
-        });
-
-        if (response.ok) {
-          const user = await response.json();
-          setUserData(user);
-        } else {
-          console.error('Failed to fetch user data:', response.statusText);
-        }
-      } catch (error) {
-        console.error('Fetch user data error:', error.message);
-      }
-    };
-
-    fetchUserData();
-  }, []); // Empty dependency array ensures the effect runs only once when the component mounts
+  const myStyle = {
+    background: `url(${process.env.PUBLIC_URL}/login.jpg)`,
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: 'cover',
+    height: '120vh',
+    marginTop: '-32px',
+  };
 
   return (
-    <div className="update-profile">
-      <h1>Profile</h1>
+    <>
+    
+    <div style={myStyle}>
+    
+      <div className={styles.update_profile}>
+        
+        <form style={{marginTop: '140px'}} action="#" encType="multipart/form-data">
+        <h1>Profile</h1>
+          <div className={styles.flex}>
+          
+            <div className={styles.inputBox}>
+              <span>Full name :</span>
+              <input type="text" name="update_name" value="#" className={styles.box} disabled />
+              <span>Email :</span>
+              <input type="email" name="update_email" value="#" className={styles.box} disabled />
+            </div>
 
-      <form action="#" encType="multipart/form-data">
-        <div className="flex">
-          <div className="inputBox">
-            <span>Full name :</span>
-            <input type="text" name="update_name" value={userData.name} className="box" disabled />
-            <span>Email :</span>
-            <input type="email" name="update_email" value={userData.email} className="box" disabled />
+            <div className={styles.inputBox}>
+              <span>Contact Number</span>
+              <input className={styles.box} value="#" disabled />
+            </div>
           </div>
 
-          <div className="inputBox">
-            <span>Contact Number</span>
-            <input value={userData.contact} className="box" disabled />
-          </div>
-        </div>
-
-        <Link to="/index" className="delete-btn">
-          Go back
-        </Link>
-      </form>
+          <Link to="/index" className={styles.delete_btn}>
+            Go back
+          </Link>
+        </form>
+      </div>
     </div>
+    </>
   );
 }
