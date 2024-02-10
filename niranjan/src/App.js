955e738c973787e7e53bed163a73750d7ad10e93
Niranjan <2100090187@kluniversity.in>
2024-02-11 02:35:31 +0530

    started implementing forget password module

--- niranjan/src/App.js
+++ niranjan/src/App.js
@@ -6,7 +6,7 @@ import Login from './components/Login';
 import Signup from './components/Signup';
 import Index from './components/index';
 import Profile from './components/Profile';
-
+import forgotPassword from './components/forgotPassword';
 function App() {
   // Add state for login user
   // eslint-disable-next-line no-unused-vars
@@ -25,6 +25,7 @@ function App() {
           <Route exact path="/login" element={<Login setLoginUser={setLoginUser} />} />
           <Route exact path="/signup" element={<Signup />} />
           <Route exact path="/profile" element={<Profile />} />
+          <Route exact path="/forget" element={<forgotPassword />} />
         </Routes>
       </BrowserRouter>
     </>
---
+++ niranjan/src/components/forgotPassword.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function forgotPassword() {
+  return (
+   <>
+    <div class="card">
+        <p class="lock-icon">
+            <i class="fas fa-lock"></i>
+        </p>
+        <h2>Forgot Password?</h2>
+        <p>You can reset your Password here</p>
+        <input type="text" class="passInput" placeholder="Email address" />
+        <button>Send My Password</button>
+    </div>
+   </>
+  )
+}
