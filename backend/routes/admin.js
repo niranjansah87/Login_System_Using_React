c14cf1f34cfbc6663e232ffd205616834bd25516
Niranjan <2100090187@kluniversity.in>
2024-02-13 00:07:00 +0530

    created end point for sign up of admin

--- backend/routes/admin.js
+++ backend/routes/admin.js
@@ -14,7 +14,28 @@ app.use(express.urlencoded({ extended: false }));
 
 
 //Route 1: Create a admin account using POST "/admin". No login required
-
+router.post('/admin', async (req, res) => {
+    let success = false;
+    try {
+        const { name, email, contact, password } = req.body;
+        // If the user with this email exists then throw an error
+        let user = await User.findOne({ email });
+        if (user) {
+            return res.status(400).json({ error: "Sorry, a user with this email already exists." });
+        }
+        const salt = await bcrypt.genSalt(10);
+        const secPass = await bcrypt.hash(password, salt);
+        // Create a new user
+        user = await User.create({
+            name, email, contact, password: secPass, role: "admin"
+        });
+        success = true;
+        res.json({ success });
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Internal Server Error");
+    }
+});
 
 
 
