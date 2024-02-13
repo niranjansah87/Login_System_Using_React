a7547ed0ff6dfb77fb7600a8f0e37382f1d44c9a
Niranjan <2100090187@kluniversity.in>
2024-02-14 00:20:56 +0530

    implementing admin module

--- backend/routes/admin.js
+++ backend/routes/admin.js
@@ -14,7 +14,12 @@ app.use(express.urlencoded({ extended: false }));
 
 
 //Route 1: Create a admin account using POST "/admin". No login required
-router.post('/admin', async (req, res) => {
+router.post('/admin',[
+    body("name", "Enter a valid name").isLength({ min: 3 }),
+    body("email", "Enter a valid email").isLength({ min: 3 }),
+    body("contact", "Enter a valid contact").isLength({ min: 10 }),
+    body("password", "Enter a password").isLength({ min: 6 }),
+  ], async (req, res) => {
     let success = false;
     try {
         const { name, email, contact, password } = req.body;
@@ -41,9 +46,31 @@ router.post('/admin', async (req, res) => {
 
 
 //Route 2: Authenticate a admin using POST "/admin/login". No login required
+router.post('/admin/login', [
+    body("email", "Enter a valid email").isLength({ min: 8 }),
+    body("password", "Enter a password").isLength({ min: 8 }),
+  ],async (req, res)=> {
+    const { email, password } = req.body;
+    let success = false;
+    try {
+        const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
+      let user = await User.findOne({ email: req.body.email });
+      if (user) {
+        return res
+          .status(400)
+          .json({ error: "Sorry, a user with this email already exists." });
+      }
+    
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Internal Server Error");
+    }
 
 
 
-
+    });
 
 module.exports = router;
